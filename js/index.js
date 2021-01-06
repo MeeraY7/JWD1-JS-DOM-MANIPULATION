8d1b7e3a0e8bfda92afeefc31b9d70feac8bf914
Michael Ahearn <mahearnpad@gmail.com>
2021-01-06 13:10:24 +1100

    working version with basic pagination and end of records check

--- js/index.js
+++ js/index.js
@@ -7,13 +7,18 @@ document.addEventListener('DOMContentLoaded', function () {
     2) Then show the list of users from the 2nd endpoint, 
       starting at https://reqres.in/api/users?page=1
     3) Create pagination buttons for previous and next, use the query string '?page=' to show the relevant page of results
+    4) Only show the pagination buttons if there are results on the page.
   */
 
   // This endpoint produces a successful login - returns a token
   const urlLogin = 'https://reqres.in/api/login';
   // This endpoint needs the query string ?page=n to work. Use it to paginate the data.
-  const urlUsers = 'https://reqres.in/api/users?page=1';
+  const urlUsers = 'https://reqres.in/api/users';
   const token = document.querySelector('#token');
+  const userList = document.querySelector('#results');
+  const btnPrevious = document.querySelector('#btnPrevious');
+  const btnNext = document.querySelector('#btnNext');
+  let currentPage = 1;
 
   //form submit button
   const loginButton = document.querySelector('#button');
@@ -38,14 +43,15 @@ document.addEventListener('DOMContentLoaded', function () {
         // this is where the action happens
         console.log(response);
         // Grab the token from response and inject it into the badge element (#token) on the page
-
+        token.innerText = response.data.token;
         // Get the full list of users on the server
         axios({
           method: 'get',
-          url: urlUsers,
+          url: `${urlUsers}?page=1`,
         }).then((response) => {
           console.log(response);
           // call the displayResults function, passing in the response object
+          displayResults(response.data.data);
         });
       })
       .catch((err) => {
@@ -54,10 +60,57 @@ document.addEventListener('DOMContentLoaded', function () {
       });
   });
 
+  /*{id: 1, email: "george.bluth@reqres.in", first_name: "George", last_name: "Bluth", 
+  avatar: "https://reqres.in/img/faces/1-image.jpg"} */
+
   const displayResults = (results) => {
     /*
       Map over the data array (see Array.prototype.map) and create an ES6 template literal which you will 
       inject into the #results element using innerHTML.
     */
+    console.log(results);
+    let resultsHTML = '';
+    results.map((user) => {
+      resultsHTML += `
+        <div class="list-group-item">
+          <p>${user.first_name} ${user.last_name}</p>
+        </div>
+      `;
+    });
+    userList.innerHTML = resultsHTML;
   };
+
+  btnPrevious.addEventListener('click', () => {
+    // decrement currentPage
+    if (currentPage > 1) {
+      --currentPage;
+    } else {
+      currentPage = 1;
+    }
+    axios({
+      method: 'get',
+      url: `${urlUsers}?page=${currentPage}`,
+    }).then((response) => {
+      console.log(response);
+      // call the displayResults function, passing in the response object
+      displayResults(response.data.data);
+    });
+  });
+
+  btnNext.addEventListener('click', () => {
+    // increment currentPage
+    ++currentPage;
+    axios({
+      method: 'get',
+      url: `${urlUsers}?page=${currentPage}`,
+    }).then((response) => {
+      console.log(response);
+      // call the displayResults function, passing in the response object
+      if (response.data.data.length > 0) {
+        displayResults(response.data.data);
+      } else {
+        userList.innerHTML = '<p>End of records</p>';
+      }
+    });
+  });
 });
